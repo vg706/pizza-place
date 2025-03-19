53fd2db5d92792eb0a837b8c0e9c871447e8c021
viniciusgjusta <viniciusxime@gmail.com>
2025-03-19 14:37:10 +0000

    refactor: server.js get

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const PORT = process.env.PORT || 8080;
 const path = require('path')
 const app = express();
 
-// app.get ("/", (req, res) => {
-//     res.json({message: "App is running on docker container"});
-// });
-app.use(express.static(path.join(__dirname, 'public')));
+app.get ("/", (req, res) => {
+    // res.json({message: "App is running on docker container"});
+    res.json('/app/produtos/index.html')
+});
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`App running on ${PORT}`);
