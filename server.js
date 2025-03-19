51d49afcf57c521390281842b6e4d2e8f027952e
viniciusgjusta <viniciusxime@gmail.com>
2025-03-19 14:31:26 +0000

    refactor: server.js modificado para servir paginas criadas

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 const PORT = process.env.PORT || 8080;
+const path = require('path')
 const app = express();
 
-app.get ("/", (req, res) => {
-    res.json({message: "App is running on docker container"});
-});
+// app.get ("/", (req, res) => {
+//     res.json({message: "App is running on docker container"});
+// });
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`App running on ${PORT}`);
