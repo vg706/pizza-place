18abe74a08a41685b1e2e85d5cb535f1b7cbf869
viniciusgjusta <viniciusxime@gmail.com>
2025-03-20 17:22:51 +0000

    refactor: tentando rodar scripts no servidor

--- Dockerfile
+++ Dockerfile
@@ -15,4 +15,4 @@ COPY . .
 
 EXPOSE 8080
 
-CMD ["node", "server.js"]
+CMD ["node", "server.js", "app.js"]
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import path from 'path';
 import { fileURLToPath } from 'url';
-// import Parse from 'parse/node.js';
+import Parse from 'parse/node.js';
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -9,8 +9,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Parse.initialize("HOa7pu3hNfi3xCUSkKzclVZl4XtxwHjlb5odaGdO", "xwV9q0OLSs0fIKYJqqUJsHPrNqPLVsSLAB0DkoZc");
-// Parse.serverURL = 'https://parseapi.back4app.com';
+
   
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'produtos', 'index.html'));
