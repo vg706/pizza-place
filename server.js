438b441b3973e4318632a15d4ed7c204ca78b3f0
viniciusgjusta <viniciusxime@gmail.com>
2025-03-20 17:43:54 +0000

    tentando rodar arquivos js no servidor

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import path from 'path';
 import { fileURLToPath } from 'url';
-import Parse from 'parse/node.js';
+// import Parse from 'parse/node.js';
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -9,8 +9,8 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-Parse.initialize("HOa7pu3hNfi3xCUSkKzclVZl4XtxwHjlb5odaGdO", "xwV9q0OLSs0fIKYJqqUJsHPrNqPLVsSLAB0DkoZc");
-Parse.serverURL = 'https://parseapi.back4app.com';
+// Parse.initialize("HOa7pu3hNfi3xCUSkKzclVZl4XtxwHjlb5odaGdO", "xwV9q0OLSs0fIKYJqqUJsHPrNqPLVsSLAB0DkoZc");
+// Parse.serverURL = 'https://parseapi.back4app.com';
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'produtos', 'index.html'));
@@ -20,17 +20,4 @@ app.listen(PORT, () => {
     console.log(`App running on ${PORT}`);
 });
 
-async function create() {
-    const player = new Parse.Object('Player');
-    player.set('name', 'Alex');
-    player.set('yearOfBirth', 1997);
-    player.set('emailContact', 'alex@email.com');
-    player.set('attributes', ['fast', 'good endurance']);
-    
-    try {
-        const result = await player.save();
-        console.log('New object created with ID:', result.id);
-    } catch (error) {
-        console.error('Failed to save object:', error.message);
-    }
-}
+app.use(express.static(path.join(__dirname, 'produtos')));
