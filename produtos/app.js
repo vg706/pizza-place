5e8fba49ae5cd1b4c9045843e64c693a02b7cbec
viniciusgjusta <viniciusxime@gmail.com>
2025-03-31 11:34:06 +0000

    feat: logica de coletar dados do banco de dados

--- produtos/app.js
+++ produtos/app.js
@@ -1,18 +1,36 @@
 Parse.initialize("HOa7pu3hNfi3xCUSkKzclVZl4XtxwHjlb5odaGdO", "xwV9q0OLSs0fIKYJqqUJsHPrNqPLVsSLAB0DkoZc");
 Parse.serverURL = 'https://parseapi.back4app.com';
 
-async function create() {
-    const player = new Parse.Object('Player');
-    player.set('name', 'Alex');
-    player.set('yearOfBirth', 1997);
-    player.set('emailContact', 'alex@email.com');
-    player.set('attributes', ['fast', 'good endurance']);
+// async function create() {
+//     const player = new Parse.Object('Player');
+//     player.set('name', 'Alex');
+//     player.set('yearOfBirth', 1997);
+//     player.set('emailContact', 'alex@email.com');
+//     player.set('attributes', ['fast', 'good endurance']);
     
+//     try {
+//         const result = await player.save();
+//         console.log('New object created with ID:', result.id);
+//     } catch (error) {
+//         console.error('Failed to save object:', error.message);
+//     }
+// }
+
+async function retrieveProduct() {
+    const query = new Parse.Query("Produto");
     try {
-        const result = await player.save();
-        console.log('New object created with ID:', result.id);
+        const product = await query.get("raTBTAffFA");
+        document.getElementById("productList").innerHTML = "<h3>"+ product.get("nomeProduto")+"</h>";
+        // console.log(product.get("nomeProduto"));
+        // console.log(product.get("precoProduto"));
+        // console.log(product.get("tamanhoPizza"));
     } catch (error) {
-        console.error('Failed to save object:', error.message);
+        console.error(error.message);
     }
 }
-document.getElementById("createButton").addEventListener("click", async function () {create();});
+window.onload = function() {
+    retrieveProduct()
+};
+// document.getElementById("createButton").addEventListener("click", async function () {create();});
+// document.getElementById("showProducts").addEventListener("click", async function () {retrieveProduct();});
+// document.getElementById("productList").addEventListener("")
